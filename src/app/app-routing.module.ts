83e46d2c103de1c7b67a2645fa1942855499dcfc
Michael Scuteri <michaelscuteri@Michaels-MacBook-Air.local>
2023-04-16 22:28:20 -0400

    add cache to gitignore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { OtherComponent } from './pages/other/other.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'other', component: HomeComponent },
+  { path: 'other', component: OtherComponent },
   { path: '', component: HomeComponent },
 ];
 
